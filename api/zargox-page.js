9300adfe8cefa2e5b57191c102a1d3bc6bc70235
Daniel Ioni <rossieugenio17@gmail.com>
2026-08-21 15:56:29 +0200

    feat: add Zargox public page handler

---
+++ api/zargox-page.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function handler(req, res) {
+  if (req.method !== 'GET') return res.status(405).send('Method not allowed');
+  const html = fs.readFileSync(path.join(__dirname, 'zargox.html'), 'utf8');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
+  return res.status(200).send(html);
+};
